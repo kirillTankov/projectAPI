b7cad0b0960228ea99703763733c4f45471c18c5
Kirill Tankov <nolixqwerty@mail.ru>
2024-04-02 19:04:35 +0300

    В процессе добавление тестов с телом запросов

--- features/steps/step_bdd_test_api.py
+++ features/steps/step_bdd_test_api.py
@@ -12,18 +12,16 @@ def step_when_get_page_endpoint(context, endpoint):
     context.response = requests.get(context.url + endpoint)
 
 
-@then('Пользователь должен получить ответ со статус кодом 200')
-def step_then_get_test_status_code(context):
-    assert context.response.status_code == 200
+@When('Пользователь отправляет тело запроса: {request_body}')
+def step_when_send_request_body(context, request_body):
+    context.request_body = request_body
+
+
+@then('Пользователь должен получить ответ со статус кодом: "{code}"')
+def step_then_get_test_status_code(context, code):
+    assert context.response.status_code == eval(code)
 
 
 @then('Пользователь должен получить следующий JSON ответ: {expected_response_body}')
 def step_then_get_expected_resnonse_body(context, expected_response_body):
     assert context.response.json() == eval(expected_response_body)
-
-
-@then('Пользователь должен получить ответ со статус кодом 404')
-def step_then_get_test_status_code(context):
-    assert context.response.status_code == 404
-
-
